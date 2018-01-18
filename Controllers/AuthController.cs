b415ae663b64e75d48dafee404dc2852bad23901
André Lopes <dx_andrelopes@hotmail.com>
2018-01-18 14:58:43 +0000

    Error throw hotfix

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -52,8 +52,6 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody]UserForLoginDto userForLoginDto)
         {
-            throw new Exception("Computer says no!");
-
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
