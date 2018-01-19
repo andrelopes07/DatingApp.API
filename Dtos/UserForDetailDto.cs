3442650088ae6a09559a9978f934e91d8b2dc761
André Lopes <dx_andrelopes@hotmail.com>
2018-01-19 03:17:22 +0000

    Extended API

--- Dtos/UserForDetailDto.cs
+++ Dtos/UserForDetailDto.cs
@@ -1,18 +1,15 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
+using DatingApp.API.Models;
 
-namespace DatingApp.API.Models
+namespace DatingApp.API.Dtos
 {
-    public class User
+    public class UserForDetailDto
     {
         public int Id { get; set; }
         public string Username { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
-
         public string Gender { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
@@ -21,13 +18,8 @@ namespace DatingApp.API.Models
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
+        public string PhotoUrl { get; set; }
 
-        public ICollection<Photo> Photos { get; set; }
-
-        public User()
-        {
-            Photos = new Collection<Photo>();
-        }
-
+        public ICollection<PhotosForDetailedDto> photos { get; set; }
     }
 }
---
+++ Helpers/AuthoMapperProfiles.cs
@@ -0,0 +1,31 @@
+using System.Linq;
+using AutoMapper;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Helpers
+{
+    public class AuthoMapperProfiles : Profile
+    {
+        public AuthoMapperProfiles()
+        {
+            CreateMap<User, UserForListDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => {
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                })
+                .ForMember(dest => dest.Age, opt => {
+                    opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+                });
+
+            CreateMap<User, UserForDetailDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => {
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                })
+                .ForMember(dest => dest.Age, opt => {
+                    opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+                });
+
+            CreateMap<Photo, PhotosForDetailedDto>();
+        }
+    }
+}
